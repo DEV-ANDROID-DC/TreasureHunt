f48c1ec3e43b51705d91bfec6d891ef55daea4f4
Debintom9633524015 <debintomdt@gmail.com>
2020-06-28 22:29:50 +0100

    Initial commit

---
+++ app/src/main/java/com/debin/treasurehunt/notifications/NotificationUtils.kt
@@ -0,0 +1,72 @@
+
+package com.debin.treasurehunt.notifications
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.debin.treasurehunt.R
+import com.debin.treasurehunt.ui.TreasureHuntActivity
+import com.debin.treasurehunt.util.GeofencingConstants
+
+fun createChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            context.getString(R.string.channel_name),
+
+            NotificationManager.IMPORTANCE_HIGH
+        )
+            .apply {
+                setShowBadge(false)
+            }
+
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        notificationChannel.enableVibration(true)
+        notificationChannel.description = context.getString(R.string.notification_channel_description)
+
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+}
+
+fun NotificationManager.sendGeofenceEnteredNotification(context: Context, foundIndex: Int) {
+    val contentIntent = Intent(context, TreasureHuntActivity::class.java)
+    contentIntent.putExtra(GeofencingConstants.EXTRA_GEOFENCE_INDEX, foundIndex)
+    val contentPendingIntent = PendingIntent.getActivity(
+        context,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    val mapImage = BitmapFactory.decodeResource(
+        context.resources,
+        R.drawable.map_small
+    )
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(mapImage)
+        .bigLargeIcon(null)
+
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setContentTitle(context.getString(R.string.app_name))
+        .setContentText(context.getString(R.string.content_text,
+            context.getString(GeofencingConstants.LANDMARK_DATA[foundIndex].name)))
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setContentIntent(contentPendingIntent)
+        .setSmallIcon(R.drawable.map_small)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(mapImage)
+
+    notify(NOTIFICATION_ID, builder.build())
+}
+
+private const val NOTIFICATION_ID = 33
+private const val CHANNEL_ID = "GeofenceChannel"
+
+
